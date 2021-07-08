e0a71540ad9c48fc80c43615f9d39061167b3530
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-07-08 18:40:38 +0200

    Got the game to work successfully and added/modified comments. Next I have to integrate loops and a GUI Once I reach the lessons on The Odin Project

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-/* Function for computerPlay */
+/* Function for generating a random choice or the computer */
 function computerChoice() {
 
-/* Generate random value that corresponds to rock paper or scissors  */
+/* Return random value that corresponds to rock paper or scissors*/
    random = Math.floor(Math.random() * 3);
    if (random == 0) {
        return "rock";
@@ -12,7 +12,7 @@ function computerChoice() {
    }
 }
 
- /* Function that runs one round and declares the winner*/
+/* Function that runs one round and declares the winner*/
 function oneRound(userChoice, computerChoice) {
 
     /* Check if user wins and declare if user wins*/
@@ -20,51 +20,132 @@ function oneRound(userChoice, computerChoice) {
          (userChoice == "scissors" && computerChoice == "paper") ||
          (userChoice == "paper" && computerChoice == "rock")) {
              alert("You win this round!")
-             return userScore++
+             return "win"
 
          /* Check if it's a tie and declare tie*/
          } else if (userChoice === computerChoice){
             alert("This round is a tie")
-            return 
+            return "tie"
 
          /* Otherwise computer wins and declare if computer wins */
          } else {
              alert("Computer wins this round")
-             return compScore++
+             return "lose"
          }
-        
-
 }
 
-
-/*let userChoice = window.prompt("Choose your weapon: Rock, Paper or Scissors").trim();
-    alert(" You have chosen: " + userChoice)
-    userChoice = userChoice.toLowerCase();
-
-compChoice = computerChoice()
-
-oneRound(userChoice, compChoice);*/
-
 /* Create a function called game() to play 5 rounds and keep score */
-    
 function game() {
     
+    /* Create variables to keep track of the score*/
     let userScore = 0
     let compScore = 0
 
-    /* Receive input from user and make sure it is case insensitive value*/
-    let userChoice = window.prompt("Choose your weapon: Rock, Paper or Scissors").trim();
-    alert(" You have chosen: " + userChoice)
-    userChoice = userChoice.toLowerCase();
+      /* Receive input from user and make sure it is case insensitive value*/
+      let userChoice = window.prompt("Choose your weapon: Rock, Paper or Scissors").trim();
+      alert(" You have chosen: " + userChoice)
 
-    compChoice = computerChoice()
+      /* Assign userChoice and compChoice*/
+      userChoice = userChoice.toLowerCase();
+  
+      compChoice = computerChoice()
+      
+      /* Play one round and return the result*/
+      result = oneRound(userChoice, compChoice);
 
-    oneRound(userChoice, compChoice);
+      /* Assign a point to the winner of the round and do nothing if its a tie */
+      if (result === "win") {
+          userScore++;
+      } else if (result === "lose") {
+          compScore++;
+      }
+      
+      /*Let user know the current score */
+      alert(`Current score is: ${userScore} to ${compScore}`)  
+
+      /* Repeat everything 5 times */
+
+      /* Repeat*/
+      userChoice = window.prompt("Choose your weapon: Rock, Paper or Scissors").trim();
+      alert(" You have chosen: " + userChoice)
+      
+      userChoice = userChoice.toLowerCase();
+  
+      compChoice = computerChoice()
+  
+      result = oneRound(userChoice, compChoice);
+  
+      if (result === "win") {
+          userScore++;
+      } else if (result === "lose") {
+          compScore++;
+      }
+  
+      alert(`Current score is: ${userScore} to ${compScore}`)
+
+      /* Repeat*/
+      userChoice = window.prompt("Choose your weapon: Rock, Paper or Scissors").trim();
+      alert(" You have chosen: " + userChoice)
+      
+      userChoice = userChoice.toLowerCase();
+  
+      compChoice = computerChoice()
+  
+      result = oneRound(userChoice, compChoice);
+  
+      if (result === "win") {
+          userScore++;
+      } else if (result === "lose") {
+          compScore++;
+      }
+  
+      alert(`Current score is: ${userScore} to ${compScore}`)
+
+      /* Repeat*/
+      userChoice = window.prompt("Choose your weapon: Rock, Paper or Scissors").trim();
+      alert(" You have chosen: " + userChoice)
+      
+      userChoice = userChoice.toLowerCase();
+  
+      compChoice = computerChoice()
+  
+      result = oneRound(userChoice, compChoice);
+  
+      if (result === "win") {
+          userScore++;
+      } else if (result === "lose") {
+          compScore++;
+      }
+  
+      alert(`Current score is: ${userScore} to ${compScore}`)
+
+      /* Repeat*/
+      userChoice = window.prompt("Choose your weapon: Rock, Paper or Scissors").trim();
+      alert(" You have chosen: " + userChoice)
+      
+      userChoice = userChoice.toLowerCase();
+  
+      compChoice = computerChoice()
+  
+      result = oneRound(userChoice, compChoice);
+  
+      if (result === "win") {
+          userScore++;
+      } else if (result === "lose") {
+          compScore++;
+      }
+        
+            alert(`Current score is: ${userScore} to ${compScore}`)
+      /* Verify final result of the game and declare it*/
+      if (userScore > compScore) {
+          alert("You are the Grand Champion!")
+      } else if (userScore == compScore) {
+          alert("It's a tie")
+      } else {
+          alert("You lose. Better luck next time!")
+      }
+
 }
 
+/*Run game() function */
 game()
-    /* Repeat everything 5 times */
-    
-    /* Keep track of score */
-
-        /* Declare the champion */
