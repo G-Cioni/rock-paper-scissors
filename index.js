b75b11be256e51d523aa006ed920f5b9b131ffed
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-07-09 02:07:30 +0200

    united many prompts together. Now game also provides option to play again. information on rounds is shown to the player

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* Function for generating a random choice or the computer */
+/* Function for generating a random choice for the computer */
 function computerChoice() {
 
 /* Return random value that corresponds to rock paper or scissors*/
@@ -12,62 +12,61 @@ function computerChoice() {
     }
 }
 
-/* Function that runs one round and declares the winner*/
+/* Function that runs one round*/
 function oneRound(userChoice, computerChoice) {
 
-    /* Check if user wins and declare if user wins*/
+    /* Check if user wins*/
     if ((userChoice == "rock" && computerChoice == "scissors") ||
          (userChoice == "scissors" && computerChoice == "paper") ||
          (userChoice == "paper" && computerChoice == "rock")) {
-            alert("You win this round!");
             return "win";
-
-         /* Check if it's a tie and declare tie*/
+         /* Check if it's a tie*/
          } else if (userChoice === computerChoice){
-            alert("This round is a tie");
             return "tie";
-
-         /* Otherwise computer wins and declare if computer wins */
+         /* Otherwise computer wins */
          } else {
-            alert("Computer wins this round");
             return "lose";
          }
 }
 
-/* Create a function called game() to play 5 rounds and keep score */
+/* Function to run 5 rounds and keep score */
 function game() {
     
-    /* Create variables to keep track of the score*/
+    /* Variables to keep track of the score */
     let userScore = 0
     let compScore = 0
 
-    /*Loop to run 5 rounds */
-    for (i=0; i<4; i++) {
+    /* Loop to run 5 rounds */
+    for (round = 1; round < 6; round++) {
 
         /* Receive input from user and make sure it is case insensitive value*/
-        let userChoice = window.prompt("Choose your weapon: Rock, Paper or Scissors").trim();
-        alert(" You have chosen: " + userChoice);
+        let userChoice = window.prompt(`| Round ${round}/5 |
+Choose your weapon: Rock, Paper or Scissors`).trim();
   
-        /* Assign userChoice and compChoice*/
+        /* Assign userChoice and compChoice and let the user know*/
         userChoice = userChoice.toLowerCase();
     
         compChoice = computerChoice();
+
+        alert(`You choose: ${userChoice.toUpperCase()} | Computer chooses: ${compChoice.toUpperCase()}`);
         
-        /* Play one round and return the result*/
+        /* Play one round and return the result */
         result = oneRound(userChoice, compChoice);
   
-        /* Assign a point to the winner of the round and do nothing if its a tie */
+        /* Assign a point to the winner and update user on current score */
         if (result === "win") {
             userScore++;
+            alert(`You win round ${round}! Current score is: ${userScore} to ${compScore}`);
         } else if (result === "lose") {
             compScore++;
+            alert(`You lose round ${round}. Current score is: ${userScore} to ${compScore}`);
+        } else {
+            alert(`Round ${round} is a tie. Current score is: ${userScore} to ${compScore}`);
         }
         
-        /*Let user know the current score */
-        alert(`Current score is: ${userScore} to ${compScore}`)  ;
         }  
         
-        /* Declare the winner*/
+        /* Declare the winner */
         if (userScore > compScore) {
             alert("You are the Grand Champion!");
         } else if (userScore == compScore) {
@@ -76,7 +75,15 @@ function game() {
             alert("You lose. Better luck next time!");
         }
 
+        /* Ask user to play again */
+        replay = window.prompt("Play again?: ").trim();
+        replay = replay.toLowerCase();
+        if (replay == "yes") {
+            game();
+        } else {
+            alert("Thanks for playing")
+        }
 }
 
-/*Run the game*/
+/* Start the game */
 game()
