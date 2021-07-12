2e770c1f2e3045b23be3f9b49ef4ca400ce0666d
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-07-13 01:28:54 +0200

    finished project for the most part

--- index.js
+++ index.js
@@ -1,34 +1,56 @@
+
+    var userScore = 0;
+
+    var compScore = 0;
+
+    var currentRound = 0;
+    
+    
+
+    let score = document.querySelector("#score")
+    let round = document.querySelector("#round")
+    let result = document.querySelector("#result")
+    score.textContent = `Current score: ${userScore} - ${compScore}`
+    round.textContent = `Round ${currentRound}`
+    
 /* Function for generating a random choice for the computer */
 function computerSelection() {
-/* Return random value that corresponds to rock paper or scissors*/
-   random = Math.floor(Math.random() * 3);
-    if (random == 0) {
-        return "rock";
-    } else if (random == 1) {
-        return "paper";
-    } else {
-        return "scissors";
+    /* Return random value that corresponds to rock paper or scissors*/
+       random = Math.floor(Math.random() * 3);
+        if (random == 0) {
+            return "rock";
+        } else if (random == 1) {
+            return "paper";
+        } else {
+            return "scissors";
+        }
     }
-}
-
-
-let userScore = 0
-
-let compScore = 0
+    
 
 /* Function that runs one round*/
 function oneRoundRock() {
     computerChoice = computerSelection()
     /* Check if user wins*/
-    if (computerChoice == "scissors") {
+    if (computerChoice === "scissors") {
             console.log("win")
+            userScore++
+            score.textContent = `Current score: ${userScore}-${compScore}`
+            currentRound++
+            round.textContent = `Round ${currentRound}`
             return "win";
          /* Check if it's a tie*/
          } else if (computerChoice === "rock"){
             console.log("tie")
+            score.textContent = `Current score: ${userScore}-${compScore}`
+            currentRound++
+            round.textContent = `Round ${currentRound}`
             return "tie";
          /* Otherwise computer wins */
          } else {
+            compScore++
+            score.textContent = `Current score: ${userScore}-${compScore}`
+            currentRound++
+            round.textContent = `Round ${currentRound}`
             console.log("lose")
             return "lose";
          }
@@ -38,15 +60,25 @@ function oneRoundRock() {
 function oneRoundPaper() {
     computerChoice = computerSelection()
     /* Check if user wins*/
-    if (computerChoice == "rock") {
-            console.log("win")
-            return "win";
+    if (computerChoice === "rock") {
+        console.log("win")
+        userScore++
+        score.textContent = `Current score: ${userScore}-${compScore}`
+        currentRound++
+        round.textContent = `Round ${currentRound}`
+        return "win";
          /* Check if it's a tie*/
          } else if (computerChoice === "paper"){
             console.log("tie")
+            currentRound++
+            round.textContent = `Round ${currentRound}`
             return "tie";
          /* Otherwise computer wins */
          } else {
+            compScore++
+            score.textContent = `Current score: ${userScore}-${compScore}`
+            currentRound++
+            round.textContent = `Round ${currentRound}`
             console.log("lose")
             return "lose";
          }
@@ -56,40 +88,66 @@ function oneRoundPaper() {
 function oneRoundScissors() {
     computerChoice = computerSelection()
     /* Check if user wins*/
-    if (computerChoice == "paper") {
+    if (computerChoice === "paper") {
             console.log("win")
+            userScore++
+            score.textContent = `Current score: ${userScore}-${compScore}`
+            currentRound++
+            round.textContent = `Round ${currentRound}`
             return "win";
          /* Check if it's a tie*/
          } else if (computerChoice === "scissors"){
             console.log("tie")
+            currentRound++
+            round.textContent = `Round ${currentRound}`
             return "tie";
          /* Otherwise computer wins */
          } else {
+            compScore++
+            score.textContent = `Current score: ${userScore}-${compScore}`
+            currentRound++
+            round.textContent = `Round ${currentRound}`
             console.log("lose")
             return "lose";
          }
 }
 
 
-
+function checkWinner() {
+    if (userScore == 5) {
+        result.textContent = "You are the Gand Champion!";
+        userScore = 0;
+        compScore = 0;
+        currentRound = 0;
+    } else if (compScore == 5){
+        result.textContent = "You lose! Better luck next time.";
+        userScore = 0;
+        compScore = 0;
+        currentRound = 0;
+    
+    } else {
+        result.textContent = "";
+    }
+}
 
 
     
     let rock = document.querySelector("#rock") 
 
     rock.addEventListener("click", () => {oneRoundRock()});
+    rock.addEventListener("click", () => {checkWinner()});
 
     let paper = document.querySelector("#paper") 
 
     paper.addEventListener("click", () => {oneRoundPaper()});
+    paper.addEventListener("click", () => {checkWinner()});
 
     let scissors = document.querySelector("#scissors") 
 
     scissors.addEventListener("click", () => {oneRoundScissors()});
+    scissors.addEventListener("click", () => {checkWinner()});
 
-
-
-// /* Function to run 5 rounds and keep score */
+// /* Function to r rounds and keep score */
 // function game() {
     
 //     /* Variables to keep track of the score */
@@ -97,14 +155,14 @@ function oneRoundScissors() {
 
 //     let compScore = 0
 
-//     /* Loop to run 5 rounds */
+//     /* Loop to r rounds */
 //     for (round = 1; round < 6; round++) {
 
 //         /* Receive input from user and make sure it's always valid*/
 //         let userChoice
 
 //         while (userChoice !== "rock" && userChoice !== "scissors" && userChoice !== "paper") {
-//         userChoice = window.prompt(`| Round ${round}/5 |
+//         userChoice = window.prompt(`| Round ${roun |
 // Choose your weapon: Rock, Paper or Scissors`).trim().toLocaleLowerCase();
 //         } 
   
